23641ae7b4b77b535f0465d54f9b25c167768a33
krissemicolon <krissemicolon@gmail.com>
2021-03-01 19:34:41 +0100

    input put

--- src/Input/input.cpp
+++ src/Input/input.cpp
@@ -2,24 +2,24 @@
 #include <string>
 #include <GLFW/glfw3.h>
 
+#include "input.hpp"
+
 using namespace std;
+using namespace corium;
 
-namespace corium 
+Input::Input() 
 {
-    class input {
 
-    };
+}
 
-    class Keyboard 
-    {
-        int keyCallback = glfwSetKeyCallback(window, key_callback);
-        // Key States: GLFW_KEY_<KEYCODE>
-        int state = glfwGetKey(window, GLFW_KEY_E);
+Keyboard::Keyboard()
+{
+    int keyCallback = glfwSetKeyCallback(window, key_callback);
+    // Key States: GLFW_KEY_<KEYCODE>
+    int state = glfwGetKey(window, GLFW_KEY_E);
+}
 
-    };
+Mouse::Mouse() 
+{
 
-    class Mouse
-    {
-        
-    };
 }
--- src/Input/input.hpp
+++ src/Input/input.hpp
@@ -1,6 +1,25 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+namespace corium
+{
+    class Input
+    {
+    public:
+        Input();
+    };
 
+    class Keyboard
+    {
+    public:
+        Keyboard();
+    };
+
+    class Mouse
+    {
+    public:
+        Mouse();
+    };
+}
 
 #endif
