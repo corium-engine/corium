602ac58b4f01a02963d0a01003ed9fa578ff0197
lianstuder <ln.studer@protonmail.ch>
2021-03-02 10:46:01 +0100

    remove redundant constructor/destructor definition

--- src/Input/input.hpp
+++ src/Input/input.hpp
@@ -6,18 +6,12 @@ namespace corium
     class Keyboard
     {
     public:
-        Keyboard();
-        ~Keyboard();
-
         void setKeyCallback(GLFWwindow *, GLFWkeyfun);
     };
 
     class Mouse
     {
     public:
-        Mouse();
-        ~Mouse();
-
         void setCursorPosCallback(GLFWwindow *, GLFWcursorposfun);
         void inputMode(GLFWwindow *);
         void createCursor(int shape);
